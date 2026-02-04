faab6d8b793f4e40bd31792b49bbf269aef2fd27
Attila <szszattila2@gmail.com>
2026-02-04 17:52:21 +0200

    add: randInt helper

--- main.go
+++ main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"os"
+	"fmt"
+	"math/rand"
 )
 
 type KV interface {
 	Get(key []byte) (val []byte, ok bool)
 	Set(key []byte, val []byte)
-	Del(key []byte)FindGreaterThan(key []byte) Iterator
+	Del(key []byte)
+	FindGreaterThan(key []byte) Iterator
 }
 
 type Iterator interface {
 	HasNext() bool
 	Next() (key []byte, val []byte)
+}
+
+func randomInt() int64 {
+	return rand.Int63()
 }
 
 func SaveData1(path string, data []byte) error {
@@ -29,3 +36,26 @@ func SaveData1(path string, data []byte) error {
 	return  fp.Sync()
 }
 
+// fix SaveData2
+func SaveData2(path string, data []byte) error {
+	tmp := fmt.Sprintf("%s.tmp.%d", path, randomInt())
+	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0664)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		fp.Close()
+		if err != nil {
+			os.Remove(tmp)
+		}
+	}()
+
+	if _, err = fp.Write(data); err != nil {
+		return err
+	}
+	if err = fp.Sync(); err != nil {
+		return err
+	}
+	err = os.Rename(tmp, path)
+	return err
+}
